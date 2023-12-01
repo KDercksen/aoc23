04c1f31e0ef0b58bf7a4f21f7f15494e8912e74d
KDercksen <mail@koendercksen.com>
2023-12-01 12:29:02 +0100

    solutions day one

---
+++ 1/part_one.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const lineReader = readline.createInterface({
+    input: fs.createReadStream("input.txt"),
+});
+
+var sum = 0;
+
+lineReader.on("line", (line) => {
+    const firstDigit = line.match(/\d/)[0];
+    const reverseLine = line.trim().split("").reverse().join("");
+    const lastDigit = reverseLine.match(/\d/)[0];
+    sum += parseInt(firstDigit + lastDigit);
+});
+
+lineReader.on("close", () => {
+    console.log(sum);
+});
